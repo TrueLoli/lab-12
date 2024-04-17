162d5629e3e355fab57a2b53975997792ed98343
Admin <Admin@DESKTOP-NVVN2EB>
2024-04-17 10:26:46 +0800

    finish

--- lab122/src/lab122/forbakiii.java
+++ lab122/src/lab122/forbakiii.java
@@ -4,7 +4,8 @@ public class forbakiii {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hi my name is muugy");
+		System.out.println("ynzalchihlaa shvv bro");
 	}
-	sysout(Hello Mu name Baki)
+	
 }
